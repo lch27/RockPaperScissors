1434427acd54ab1981c4d1ef5fadf6122268d49e
lch27 <mag.anderson96@gmail.com>
2023-10-04 20:06:32 +0900

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Maggie Li on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
